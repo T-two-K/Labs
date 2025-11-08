d3f229319f6de168f6d6e4f4ceb55ff099c991fc
T-TwO-K <4erepashka121@gmail.com>
2025-11-08 22:45:07 +0300

    Initial commit: Added the first two lab assignments on C#

---
+++ FirstLab-first-code-in-Visual-Studio/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace FromLabs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeThirdExercise();
+
+        }
+
+        static void MakeThirdExercise()
+        {
+            Console.Write("Введите натуральное число: ");
+            int naturalNumber = Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+            double finalResult = 1;
+
+            for (int i = 1; i < naturalNumber; i++)
+            {
+                result = result + Math.Cos(Math.Cos(i)) / Math.Sin(Math.Sin(i));
+                finalResult *= result;
+            }
+
+            Console.WriteLine($"Финальный результат: {finalResult}");
+        }
+
+        static void MakeFirstExercise()
+        {
+            Console.Write("Введите переменную X и Y:\nПеременная X: ");
+            string pointX = Console.ReadLine();
+            Console.Write("Переменная Y: ");
+            string pointY = Console.ReadLine();
+
+            if (double.TryParse(pointX, out double correctX) && double.TryParse(pointY, out double correctY))
+            {
+                if (correctX != 1)
+                {
+                    double result = Math.Pow((correctX + 1) / (correctX - 1), correctX);
+                    result += Math.Pow(18 * correctX * correctY, 2);
+                    Console.Write($"Результат вычисления: {result} ");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! На 0 делить нельзя!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введены неверные данные.");
+            }
+        }
+
+        static void MakeSecondExercisePartOne()
+        {
+            Console.Write("Введите число от 1 до 5: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("Плохо!");
+                    break;
+                case 2:
+                    Console.WriteLine("Неудовлетворительно.");
+                    break;
+                case 3:
+                    Console.WriteLine("Удовлетворительно.");
+                    break;
+                case 4:
+                    Console.WriteLine("Хорошо");
+                    break;
+                case 5:
+                    Console.WriteLine("Отлично");
+                    break;
+
+                default:
+                    Console.WriteLine("Вы ввели неверное значение!");
+                    break;
+            }
+        }
+
+        static void MakeSecondExercisePartTwo()
+        {
+            Console.WriteLine("Введите число любое число: ");
+            double pointZ = Convert.ToDouble(Console.ReadLine());
+            double pointX;
+
+            if (pointZ < 1)
+            {
+                pointX = Math.Pow(pointZ, 3) + 0.2;
+            }
+            else
+            {
+                pointX = pointZ + Math.Log(pointZ);
+            }
+
+            double pointY = Math.Pow(Math.Cos(Math.Pow(pointX, 2)), 3) + Math.Pow(Math.Sin(Math.Sqrt(Math.Pow(pointX, 3))), 2);
+
+            Console.WriteLine($"Результаты вычисления: {pointY} ");
+        }
+    }
+}
---
+++ SecondLab-arrays/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace SecondLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstTask();
+        }
+
+        static void SecondTask()
+        {
+            Random random = new();
+            double multiply = 1;
+            double sum = 0;
+
+            Console.Write("Введите длину массива: ");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            double[] ourArray = new double[arrayLength];
+            double[] arrayWithAbs = new double[arrayLength];
+
+            ourArray = FillInArray(ourArray, random);
+
+            Console.Write("\nНаш массив:\n");
+            ShowMeAnArray(ourArray);
+
+            for (int i = 0; i < arrayLength; i++)
+                arrayWithAbs[i] = Math.Abs(ourArray[i]);
+
+            var maxElement = (double)arrayWithAbs.Max(max => max);
+            Console.WriteLine("\nМаксимальный элемент по модулю: " + maxElement);
+
+            var minElement = (double)arrayWithAbs.Min(min => min);
+            Console.WriteLine("Минимальный элемент по модулю: " + minElement);
+
+            FindMinAndMaxIndexOfElements(arrayWithAbs, out int startIndex, out int endIndex, maxElement, minElement);
+
+            for (int i = 0; i < arrayLength; i++)
+                if (ourArray[i] > 0)
+                    sum += ourArray[i];
+
+            for (int i = startIndex + 1; i < endIndex - 1; i++)
+                multiply *= ourArray[i];
+
+            var sortArray = ourArray.OrderByDescending(element => element).ToArray();
+
+            Console.WriteLine($"\nСумма элементов: {Math.Round(sum, 1)}.");
+            Console.WriteLine($"произведение элементов: {multiply}.");
+
+            Console.Write("\nОтсортированный массив:\n");
+            ShowMeAnArray(sortArray);
+
+
+
+        }
+
+        static void FindMinAndMaxIndexOfElements(double[] array, out int startIndex, out int endIndex, double maxElement, double minElement)
+        {
+            var indexMaxElement = Array.IndexOf(array, maxElement);
+            var indexMinElement = Array.IndexOf(array, minElement);
+
+            if (indexMaxElement > indexMinElement)
+            {
+                startIndex = indexMinElement;
+                endIndex = indexMaxElement;
+            }
+            else
+            {
+                startIndex = indexMaxElement;
+                endIndex = indexMinElement;
+            }
+        }
+
+        static void ShowMeAnArray(double[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+
+        static double[] FillInArray(double[] array, Random rand)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-100, 101) + Math.Round(rand.NextDouble(), 1);
+            return array;
+        }
+
+        static void FirstTask()
+        {
+            Random rand = new();
+            int[] ourArray = new int[10];
+            int countOfNumber = 0;
+            int sum = 0;
+
+            ourArray = FillInArray(ourArray, rand);
+            ShowMeAnArray(ourArray);
+
+            for (int i = 0; i < ourArray.Length; i++)
+                if (ourArray[i] % 7 == 0 && ourArray[i] < 0)
+                {
+                    sum += ourArray[i];
+                    countOfNumber++;
+                }
+
+            Console.Write($"\nКоличество отрицательных чисел кратных 7: {countOfNumber}\nИх сумма: {sum}.");
+
+
+        }
+
+        static int[] FillInArray(int[] array, Random rand)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-100, 11);
+            return array;
+        }
+
+        static void ShowMeAnArray(int[] array)
+        {
+            foreach (var element in array)
+                Console.Write(element + " ");
+        }
+    }
+
+}
