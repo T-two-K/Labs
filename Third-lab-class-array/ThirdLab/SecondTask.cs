56de5ec96b0815d1c86658305bc4151c54a221ac
T-TwO-K <4erepashka121@gmail.com>
2025-11-10 22:01:36 +0300

    Added a third lab and edited the ain README: Added instructions for running the labs.

---
+++ Third-lab-class-array/ThirdLab/SecondTask.cs
@@ -0,0 +1,92 @@
+﻿namespace ThirdLab
+{
+    internal class SecondTask
+    {
+        public static void CompleteSecondTask()
+        {
+            Console.Write("Введите количество строк: ");
+            string? userInput = Console.ReadLine();
+            int cols = ConvertToInt(userInput);
+
+            Console.Write("Введите количество столбцов: ");
+            userInput = Console.ReadLine();
+            int rows = ConvertToInt(userInput);
+
+            int[,] firstArray = new int[cols, rows];
+            int[,] secondArray = new int[cols, rows];
+
+            firstArray = FillInArray(firstArray);
+            secondArray = FillInArray(secondArray);
+
+            Console.Write("\nПервый массив:\n");
+            ShowArray(firstArray);
+            Console.Write("\nВторой массив:\n");
+            ShowArray(secondArray);
+
+            int[,] arraySum = new int[cols, rows];
+            arraySum = FindArraySum(arraySum, firstArray, secondArray);
+            Console.Write("\nСумма массивов:\n");
+            ShowArray(arraySum);
+
+            Console.Write("\nТранспонированный массив:\n");
+            int[,] transparentArray = Transparent(arraySum);
+            ShowArray(transparentArray);
+        }
+
+        static int[,] FillInArray(int[,] array)
+        {
+            Random rand = new();
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    array[i, j] = rand.Next(1, 34);
+            return array;
+        }
+
+        static int[,] Transparent(int[,] array)
+        {
+            int[,] transparentArray = new int[array.GetLength(1), array.GetLength(0)];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    transparentArray[j, i] = array[i, j];
+            return transparentArray;
+        }
+
+        static int[,] FindArraySum(int[,] arraySum, int[,] firstArray, int[,] secondArray)
+        {
+            if (firstArray.Length == secondArray.Length)
+            {
+                for (int i = 0; i < firstArray.GetLength(0); i++)
+                    for (int j = 0; j < firstArray.GetLength(1); j++)
+                        arraySum[i, j] = secondArray[i, j] + firstArray[i, j];
+                return arraySum;
+            }
+            else
+            {
+                Console.WriteLine("Матрицы не суммируются.");
+                return arraySum = new int[,] { { 0 } };
+            }
+        }
+
+        static void ShowArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write(array[i, j] + " ");
+                Console.Write("\n");
+            }
+        }
+
+        static int ConvertToInt(string? str)
+        {
+            if (int.TryParse(str, out int value)) return value;
+            else
+            {
+                Console.WriteLine("Вы ввели неверные данные. Программа завершает свою работу.");
+                System.Environment.Exit(0);
+                return 0;
+            }
+        }
+    }
+}
---
+++ Third-lab-class-array/ThirdLab/ThirdTask.cs
@@ -0,0 +1,49 @@
+﻿namespace ThirdLab
+{
+    internal  class ThirdTask
+    {
+        public static void CompleteThirdTask()
+        {
+            int[][] arrays = [new int[4], new int[5], new int[3]];
+
+            int sum = 0;
+
+            arrays = FillInArray(arrays);
+
+            Console.Write("Наш ступенчатый массив:\n");
+            ShowArray(arrays);
+
+            foreach (int[] array in arrays)
+                sum += array.Sum();
+
+            Console.Write("\nСумма всех элементов массива: " + sum);
+
+            foreach (int[] array in arrays)
+                Array.Sort(array);
+
+            Console.Write("\nМассив с отсортированными элементами:\n");
+            ShowArray(arrays);
+        }
+
+        static int[][] FillInArray(int[][] array)
+        {
+            Random rand = new();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                    array[i][j] = rand.Next(1, 43);
+            }
+            return array;
+        }
+
+        static void ShowArray(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                    Console.Write(array[i][j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
